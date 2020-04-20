f6641ad1aab8d2df2dcb3091f3ee598543400499
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-04-20 06:55:01 +0100

    completed redux solution

--- src/index.js
+++ src/index.js
@@ -1,21 +1,87 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { createStore } from "redux";
+import { connect, Provider } from "react-redux";
+import "./styles.scss";
+import { bindActionCreators } from "redux";
 
-import './styles.scss';
+const initialState = {
+  count: 0,
+};
+
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
+
+const increment = () => ({
+  type: INCREMENT,
+});
+
+const decrement = () => ({
+  type: DECREMENT,
+});
+
+const reset = () => ({
+  type: RESET,
+});
+
+const reducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
+    return {
+      count: state.count + 1,
+    };
+  }
+  if (action.type === DECREMENT) {
+    return {
+      count: state.count - 1,
+    };
+  }
+  if (action.type === RESET) {
+    return {
+      count: 0,
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(reducer);
 
 class Counter extends Component {
   render() {
+    const { count, increment, decrement, reset } = this.props;
+    console.log({ count, increment, decrement, reset });
     return (
       <main className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>Decrement</button>
+          <button onClick={reset}>Reset</button>
         </section>
       </main>
     );
   }
 }
 
-render(<Counter />, document.getElementById('root'));
+const mapStateToProps = (state) => {
+  return state;
+};
+/* const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ increment, decrement, reset }, dispatch);
+}; */
+
+const mapDispatchToProps = {
+  increment,
+  decrement,
+  reset,
+};
+
+const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+render(
+  <Provider store={store}>
+    <CounterContainer />
+  </Provider>,
+  document.getElementById("root")
+);
